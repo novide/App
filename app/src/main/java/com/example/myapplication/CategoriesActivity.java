412047b94d7b9c8a13a86692be76efc9c6293812
TAEHEE JANG <76940032+novide@users.noreply.github.com>
2023-04-03 23:59:28 +0900

    바뀜

--- app/src/main/java/com/example/myapplication/CategoriesActivity.java
+++ app/src/main/java/com/example/myapplication/CategoriesActivity.java
@@ -1,6 +1,7 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
 
 import android.os.Bundle;
 
@@ -10,5 +11,9 @@ public class CategoriesActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_categories);
+
+        SearchView searchView = findViewById(R.id.search); //SearchView 객체 생성
+        String query = getIntent().getStringExtra("search_query"); //Intent로 전달된 값을 가져옴
+        searchView.setQuery(query, false); //SearchView에 입력된 값을 적용함
     }
 }
--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -5,9 +5,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 
+import android.util.Log;
+import android.view.SearchEvent;
 import android.view.View;
 import android.widget.Button;
-import android.widget.SearchView;
+import androidx.appcompat.widget.SearchView;
 
 //바꾸었어요 선생님
 public class MainActivity extends AppCompatActivity {
@@ -15,6 +17,7 @@ public class MainActivity extends AppCompatActivity {
     private Button reservation_bt;
     private Button home_bt;
 
+    private SearchView searchView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +28,23 @@ public class MainActivity extends AppCompatActivity {
         reservation_bt = findViewById(R.id.reservation_bt);
         home_bt = findViewById(R.id.home_bt);
 
+        searchView = findViewById(R.id.search);
+        String query = searchView.getQuery().toString(); //입력된 값을 가져옴
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                Intent intent = new Intent(MainActivity.this, CategoriesActivity.class);
+                intent.putExtra("search_query", query);
+                startActivity(intent);
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
         info_bt.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -48,8 +68,5 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(reservationIntent);
             }
         });
-
     }
-
-
 }
