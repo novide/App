d0c516af19598d8bc1daca2d2a5d845e282fadce
TAEHEE JANG <76940032+novide@users.noreply.github.com>
2023-04-04 00:50:12 +0900

    검색하고 이동한 페이지 뒤로가기 버튼 추가

--- app/src/main/java/com/example/myapplication/CategoriesActivity.java
+++ app/src/main/java/com/example/myapplication/CategoriesActivity.java
@@ -2,8 +2,10 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.SearchView;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class CategoriesActivity extends AppCompatActivity {
 
@@ -12,8 +14,23 @@ public class CategoriesActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_categories);
 
+        Toolbar toolbar4 = findViewById(R.id.toolbar4);
+        setSupportActionBar(toolbar4);
+        // 뒤로가기 버튼
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
         SearchView searchView = findViewById(R.id.search); //SearchView 객체 생성
         String query = getIntent().getStringExtra("search_query"); //Intent로 전달된 값을 가져옴
         searchView.setQuery(query, false); //SearchView에 입력된 값을 적용함
+    }
+    // 뒤로가기 버튼 누르면 뒤로
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == android.R.id.home) {
+            onBackPressed(); // 현재 액티비티 종료
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
