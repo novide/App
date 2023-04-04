d387e4c10040825032e37ed051055eefddd70596
TAEHEE JANG <76940032+novide@users.noreply.github.com>
2023-04-04 23:20:11 +0900

    로그아웃 추가

--- app/src/main/java/com/example/myapplication/LoginSuccess.java
+++ app/src/main/java/com/example/myapplication/LoginSuccess.java
@@ -1,10 +1,12 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
@@ -17,6 +19,11 @@ public class LoginSuccess extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_success);
+
+        Toolbar toolbar5 = findViewById(R.id.toolbar5);
+        setSupportActionBar(toolbar5);
+        // 뒤로가기 버튼
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         logoutBtn = findViewById(R.id.logout_bt2);
 
@@ -37,5 +44,14 @@ public class LoginSuccess extends AppCompatActivity {
                 finish();
             }
         });
+    }
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == android.R.id.home) {
+            onBackPressed(); // 현재 액티비티 종료
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
